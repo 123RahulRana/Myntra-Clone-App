53bd66625542d86be04e7f5390246d1440b0614c
123RahulRana <104134023+123RahulRana@users.noreply.github.com>
2024-05-25 17:40:20 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./routes/App.jsx";
+import "./index.css";
+import Bag from "./routes/Bag.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./routes/home.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
